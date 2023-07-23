41ed57c262e5d4b2f4e24690b5652c4952378593
JacobYLiu <36716000+JacobYLiu@users.noreply.github.com>
2023-07-23 14:20:53 -0500

    progress

---
+++ deliveroo-clone/components/categories.js
@@ -0,0 +1,10 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+export default function Categories () {
+  return (
+    <ScrollView>
+      <Text>categories</Text>
+    </ScrollView>
+  )
+}
+
--- deliveroo-clone/screens/HomeScreen.js
+++ deliveroo-clone/screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { UserIcon, ChevronDownIcon, SearchIcon, MagnifyingGlassIcon, AdjustmentsVerticalIcon } from "react-native-heroicons/outline"
+import { useNavigation } from '@react-navigation/native';
+import { UserIcon, ChevronDownIcon, MagnifyingGlassIcon, AdjustmentsVerticalIcon } from "react-native-heroicons/outline"
+import Categories from "../components/categories";
 
 
 const HomeScreen = () => {
@@ -20,13 +21,17 @@ const HomeScreen = () => {
                 <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
                     <Image
                         source={{
-                            uri: "https://links.papareact.com/wru",
+                            uri: "https://payload.cargocollective.com/1/15/494563/13468564/roo-03_1340_c.jpg",
                         }}
                         className="h-7 w-7 bg-gray-300 p-4 rounded-full"
                     />
                     <View className="flex-1">
-                        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-                        <Text className="font-bold text-xl">Current Location
+                        <Text className="font-bold text-gray-400 text-xs">
+                            Deliver Now!
+                        </Text>
+
+                        <Text className="font-bold text-xl">
+                            Current Location
                             <ChevronDownIcon size={20} color="#00CCBB" />
                         </Text>
                     </View>
@@ -35,7 +40,7 @@ const HomeScreen = () => {
                 </View>
 
                 {/* Search */}
-                <View className="flex-row items-center space-x-2 pb-2 mx-4">
+                <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
                     <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
                         <MagnifyingGlassIcon color="gray" size={20} />
                         <TextInput 
@@ -43,8 +48,15 @@ const HomeScreen = () => {
                             keyboardType="default" 
                         />
                     </View>
+
                     <AdjustmentsVerticalIcon color="#00CCBB" />
                 </View>
+
+                {/* Body */}
+                <ScrollView>
+                    {/* Categories */}
+                        <Categories />
+                </ScrollView>
             </Text>
         </SafeAreaView>
     );
